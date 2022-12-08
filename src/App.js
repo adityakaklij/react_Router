8d7cbdea1eed342a5096bc0f96fd6082c2b098de
Aditya K <adityakaklij11@gmail.com>
2022-12-08 16:12:10 +0530

    react router sample

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, HashRouter,} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Hackathon from './Components/Hackathon';
+import Hosthackathon from './Components/Hosthackathon';
+
 
 function App() {
   return (
     <>
-    
+      <HashRouter basename='/'>
+    <div className="App">
+       <Navbar/>
+
+      <Switch>
+
+          <Route exact path="/">
+            <Home/>
+          </Route>
+
+          <Route exact path="/Hackathon">
+            <Hackathon/>
+          </Route>
+
+          <Route exact path="/Hosthackathon">
+            <Hosthackathon/>
+          </Route>
+
+
+          
+        </Switch>
+
+    </div>
+    </HashRouter>
     </>
   );
 }
---
+++ src/Components/Hosthackathon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hosthackathon() {
+  return (
+    <div>
+        <h1>Host Hacathon</h1>
+    </div>
+  )
+}
+
+export default Hosthackathon
